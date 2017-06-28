cb60e4fbc119e24f69c508303a66d304dc4cf1bd
Gerardo <GerardoHdez>
2017-06-28 11:25:59 -0500

    change_config_vars

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,8 +7,8 @@ class Product < ApplicationRecord
                       :storage => :fog,
                       :fog_credentials => {
                          provider: 'AWS',
-                         aws_access_key_id: ENV["aws_access_key_id"],
-                         aws_secret_access_key: ENV["aws_secret_access_key"],
+                         aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+                         aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
                          region: 'us-east-2'
                       },
                       fog_directory: ENV["FOG_DIRECTORY"]
